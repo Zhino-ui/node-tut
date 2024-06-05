6f071e86ab5f5bb51134c1875d74d47e1c70147e
Maxwell Asante <zhino4u2010@gmail.com>
2024-06-05 04:53:59 -0700

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,12 @@
+//Modules
+// const {john,peter} = require('./4-name')
+const names = require('./4-name')
+const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavor')
+require('./7-mind-grenade')
+console.log(data);
+// console.log(names);
+
+// sayHi('Susan')
+// sayHi(names.john)
+// sayHi(names.peter)
